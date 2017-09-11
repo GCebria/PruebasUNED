65c7ac9358b7acb3ef7acdc4f46c65ff344af757
GCebria <gerardcebria@gmail.com>
2017-09-11 21:54:32 +0200

    footer ampliado

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+function descargarGuia(){
+	window.open("http://www2.uned.es/saludsexual/docs/GUIASALUDSEXUAL.pdf");
+}
+
+function descargarSolicitud(){
+	window.open("http://www2.uned.es/saludsexual/docs/solicitud.pdf");
+}
