251950cd2767e558cdc35ea23d85f7f2a8b371a7
GCebria <gerardcebria@gmail.com>
2017-09-30 11:32:09 +0200

    jumbotron + videoyoutube

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,18 @@ function descargarGuia(){
 function descargarSolicitud(){
 	window.open("http://www2.uned.es/saludsexual/docs/solicitud.pdf");
 }
+
+  function resizeIframe(obj) {
+    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+  }
+
+
+  var jumboHeight = $('.jumbotron').outerHeight();
+function parallax(){
+    var scrolled = $(window).scrollTop();
+    $('.bg').css('height', (jumboHeight-scrolled) + 'px');
+}
+
+$(window).scroll(function(e){
+    parallax();
+});
